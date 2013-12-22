39b39a09d7d9e4193dfa522782fab6e072790de2
Andrea Corna <andrea.corna.ac.91@gmail.com>
2013-12-22 20:29:00 +0100

    aggiunto session bean autenticazione

---
+++ TravelDreamEJB/ejbModule/it/polimi/traveldream/ejb/sessionBeans/AutenticazioneBeanImpl.java
@@ -0,0 +1,18 @@
+package it.polimi.traveldream.ejb.sessionBeans;
+
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class AutenticazioneBeanImpl
+ */
+@Stateless
+public class AutenticazioneBeanImpl implements AutenticazioneBean {
+
+    /**
+     * Default constructor. 
+     */
+    public AutenticazioneBeanImpl() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
