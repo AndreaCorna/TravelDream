fdd2059ea9ca9a14fda3a5e92456b027f692737e
Alessandro <Alessandro@Alessandro-PC>
2014-01-08 18:19:30 +0100

    aggiunto dto prenotazione pacchetto

---
+++ TravelDreamEJB/ejbModule/it/polimi/traveldream/ejb/sessionBeans/GestionePrenotazioneImpl.java
@@ -0,0 +1,18 @@
+package it.polimi.traveldream.ejb.sessionBeans;
+
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class GestionePrenotazioneImpl
+ */
+@Stateless
+public class GestionePrenotazioneImpl implements it.polimi.traveldream.ejb.sessionBeans.GestionePrenotazione {
+
+    /**
+     * Default constructor. 
+     */
+    public GestionePrenotazioneImpl() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ TravelDreamEJBClient/ejbModule/it/polimi/traveldream/ejb/sessionBeans/GestionePrenotazione.java
@@ -0,0 +1,14 @@
+package it.polimi.traveldream.ejb.sessionBeans;
+
+import it.polimi.traveldream.ejb.dto.UtenteDTO;
+
+import java.util.List;
+
+import javax.ejb.Local;
+
+@Local
+public interface GestionePrenotazione {
+	
+
+
+}
