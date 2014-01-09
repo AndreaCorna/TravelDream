a15d1fa5d59042019f64fd468aa363db57e78e31
Alessandro <Alessandro@Alessandro-PC>
2014-01-09 21:32:59 +0100

    prenotazione data model

---
+++ TravelDreamWeb/src/it/polimi/traveldream/dataModels/PrenotazioneDataModel.java
@@ -0,0 +1,38 @@
+package it.polimi.traveldream.dataModels;
+
+import java.util.List;
+
+import it.polimi.traveldream.ejb.dto.PacchettoDTO;
+import it.polimi.traveldream.ejb.dto.Prenotazione_PacchettoDTO;
+
+import javax.faces.model.ListDataModel;
+
+import org.primefaces.model.SelectableDataModel;
+
+public class PrenotazioneDataModel extends ListDataModel<Prenotazione_PacchettoDTO> implements SelectableDataModel<Prenotazione_PacchettoDTO>{
+
+	public PrenotazioneDataModel(){
+		
+	}
+	
+	public PrenotazioneDataModel(List<Prenotazione_PacchettoDTO> listaPrenotazioni){
+		super(listaPrenotazioni);
+	}
+	@Override
+	public Prenotazione_PacchettoDTO getRowData(String id) {
+		List<Prenotazione_PacchettoDTO> prenotazioni = (List<Prenotazione_PacchettoDTO>) getWrappedData(); 
+		for(Prenotazione_PacchettoDTO pacchetto:prenotazioni){
+			Integer value = new Integer(pacchetto.getId());
+			if ( value.toString().equals(id)){
+				return pacchetto;
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public Object getRowKey(Prenotazione_PacchettoDTO pacchetto) {
+		return pacchetto.getId();
+	}
+
+}
