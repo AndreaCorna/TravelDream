20eadb3bd7370b0e071f1f1d49173c0f7672bbc7
Andrea Corna <andrea.corna.ac.91@gmail.com>
2014-01-13 10:49:40 +0100

    Il controllo delle camere disponibili viene fatto guardando le date delle prenotazioni

--- TravelDreamEJB/ejbModule/it/polimi/traveldream/ejb/sessionBeans/GestionePacchettoBeanImpl.java
+++ TravelDreamEJB/ejbModule/it/polimi/traveldream/ejb/sessionBeans/GestionePacchettoBeanImpl.java
@@ -179,7 +179,7 @@ public class GestionePacchettoBeanImpl implements GestionePacchettoBean {
 	@SuppressWarnings("unchecked")
 	private boolean haCamereDisponibili(Hotel hotel, Date partenza, Date ritorno){
 		List<Prenotazione_Pacchetto> prenotazioniPac = em.createQuery("SELECT p FROM Prenotazione_Pacchetto p "
-				+ "WHERE p.aereo1.data BETWEEN :andata AND :ritorno and p.aereo2.data BETWEEN :andata AND :ritorno")
+				+ "WHERE p.dataCheckInHotel BETWEEN :andata AND :ritorno and p.dataCheckOutHotel BETWEEN :andata AND :ritorno")
 				.setParameter("ritorno", ritorno)
 				.setParameter("andata",partenza).getResultList();
 		int camereOccupate = prenotazioniPac.size();
